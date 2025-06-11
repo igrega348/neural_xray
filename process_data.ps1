8e4529a4faa1fe2462e77416c65a301a3fd2e821
Ivan Grega <grega.ivan@gmail.com>
2025-06-11 10:45:12 +0100

    process_data script

--- process_data.ps1
+++ process_data.ps1
@@ -1,37 +1,71 @@
-$pf="kel-I-20240826"
-$r="kel-I"
-$od="kel_I"
+$pf="kel-Q-20250604"
+$r="kel-Q"
+$od="kel_Q"
+$raw_dir="E:\Deshpande Dropbox\Ivan Grega\neural_xray\data\experimental\raw\$pf"
+$processed_dir="E:\Deshpande Dropbox\Ivan Grega\neural_xray\data\experimental\processed\$od"
+$temp_dir="C:\temp\neural_xray_data"
+$tiff_to_png="C:\temp\neural_xray\nerf_data\scripts\tiff_to_png.py"
+$comp_transf="C:\temp\neural_xray\nerf_data\scripts\compute_transforms.py"
+$compute_transforms=$true
 
-New-Item -ItemType Directory -Force -Path "..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\processed\$od"
+New-Item -ItemType Directory -Force -Path "$processed_dir" -Confirm
+New-Item -ItemType Directory -Force -Path "$temp_dir" -Confirm
 
 
 for ($i=0; $i -lt 1; $i++){
-    $s="{0:d2}" -f $i
+    # $s="{0:d2}" -f $i
+    $s="def"
     "Processing $s..."
-    tar -xzf "..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\raw\$pf\$r-$i.tar.gz" `
-        -C C:\temp\ 
-    # tar -xzf C:\temp\$r-$i.tar.gz -C C:\temp\
-    if ($i -lt 10) {
-        mv "C:\temp\images_$i" "C:\temp\images_$s"
-    }
+    $images_folder="images_$s"
+    # $rd=$(Get-ChildItem -Path $raw_dir -Filter "kel-cont-$exp-$nproj-$i*" -Name)
     
-    $lambda=(Get-Content "..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\raw\$pf\normalization\lambdas2.csv" -TotalCount $($i+1) -ReadCount 100)[-1]
-    python nerf_data/scripts/tiff_to_png.py --input-folder "C:\temp\images_$s" --out-fn-pattern "train_{0:04d}.png" --greyscale-fn "lambda x: $lambda" --thresh-min 36000 --thresh-max 58000
-    rm "C:\temp\images_$s\*.tif"
-    if ($i % 5 -eq 0 -and $i -ne 5) {
-        $eval='0129'
-        $xt=''
-    } else {
-        $eval='0013'
-        $xt=''
+    # mkdir "$temp_dir\$images_folder"
+    # tar -xzf "$raw_dir\$rd\kel-cont-$exp-$nproj-$i.tar.gz" `
+    #     -C "$temp_dir\$images_folder" 
+    
+    # $tmin=27000
+    # $tmax=59000
+
+    # # $lambda=(Get-Content "$raw_dir\normalization\lambdas.csv" -TotalCount $($i+1) -ReadCount 100)[-1]
+    # python $tiff_to_png --input-folder "$temp_dir\$images_folder" --out-fn-pattern "train_{0:04d}.png" --thresh-min $tmin --thresh-max $tmax #--dtype UINT16
+    # # # python $tiff_to_png --input-folder "$temp_dir\images_$s" --out-fn-pattern "train_{0:04d}.png" #--greyscale-fn "lambda x: $lambda" --thresh-min 19000 --thresh-max 58500 #--dtype UINT16
+    
+    # rm "$temp_dir\$images_folder\*.tif"
+
+    # $eval=((ls "$temp_dir\$images_folder" | Sort-Object)[-1]).Basename.Split("_")[-1]
+    # mv "$temp_dir\$images_folder\train_$eval.png" "$temp_dir\$images_folder\eval_$eval.png"
+
+    # for ($i=1; $i -lt 2867; $i++){
+    #     if ($i%28 -ne 1){
+    #         $s="{0:d4}" -f $i
+    #         rm "$temp_dir\$images_folder\train_$s.png"
+    #     }
+    # }
+
+    # cp -LiteralPath "$raw_dir\$rd\_ctdata.txt" -Destination $temp_dir
+    # cp -LiteralPath "$raw_dir\$rd\kel-cont-$exp-$nproj-$i.xtekct" -Destination $temp_dir
+    # cp -LiteralPath "$raw_dir\$rd\kel-cont-$exp-$nproj-$i.ctinfo.xml" -Destination $temp_dir
+    
+    $ff=0.89
+    if ($compute_transforms){
+        python "$comp_transf" --folder "$temp_dir" --images-folder "$images_folder" --output-fname "$temp_dir\transforms_$s.json" --deblurring Gauss --deblurring-points 1 --flat-field $ff --angles-file "$temp_dir\_ctdata-$s.txt" #--xtekct-file "$temp_dir\$r-cont-2.xtekct" 
+        # Move-Item "$temp_dir\transforms_0_to_1.json" -Destination "$processed_dir"
     }
-    cp "..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\raw\$pf\$r-$i.ang" C:\temp
-    cp "..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\raw\$pf\$r-$i$xt.xtekct" C:\temp
-    mv "C:\temp\images_$s\train_$eval.png" "C:\temp\images_$s\eval_$eval.png"
-    python .\nerf_data\scripts\compute_transforms.py --folder "C:\temp" --images-folder "images_$s" --xtekct-file "$r-$i$xt.xtekct" --angles-file "$r-$i.ang" --output-fname "C:\temp\transforms_$s.json"
-    Move-Item "C:\temp\images_$s"  -Destination "..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\processed\$od"
-    Move-Item "C:\temp\transforms_$s.json" -Destination "..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\processed\$od"
-    rm "C:\temp\*"
+    # rm "$temp_dir\kel-cont-$exp-$nproj-$i.xtekct"
+    # rm "$temp_dir\kel-cont-$exp-$nproj-$i.ctinfo.xml"
+    # rm "$temp_dir\_ctdata.txt"
+
+    # if ($compute_transforms){
+    #     cp "$raw_dir\$r-$i.ang" $temp_dir
+    #     cp "$raw_dir\$r-$i$xt.xtekct" $temp_dir
+    #     python "$comp_transf" --folder "$temp_dir" --images-folder "images_$s" --xtekct-file "$r-$i$xt.xtekct" --angles-file "$r-$i.ang" --output-fname "$temp_dir\transforms_$s.json"
+    #     Move-Item "$temp_dir\transforms_$s.json" -Destination "$processed_dir"
+    #     rm "$temp_dir\$r-$i$xt.xtekct"
+    #     rm "$temp_dir\$r-$i.ang"
+    # }
+    # Move-Item "$temp_dir\images_$s" -Destination "$processed_dir"
+
+    # rm "C:\temp\*"
     # # $pth="..\..\Cambridge University Dropbox\Ivan Grega\neural_xray\data\experimental\raw\$pf"
     # # Get-ChildItem -Path $pth -Filter *.raw -Recurse -Name | ForEach {
     # #     $fn = $_
